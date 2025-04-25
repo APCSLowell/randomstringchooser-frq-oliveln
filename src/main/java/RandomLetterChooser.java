3c526d505971f22b43527bfe9258a08ec03f3a59
oliveln <143148891+oliveln@users.noreply.github.com>
2025-04-24 21:06:57 -0700

    Update RandomLetterChooser.java

--- src/main/java/RandomLetterChooser.java
+++ src/main/java/RandomLetterChooser.java
@@ -1,11 +1,23 @@
 public class RandomLetterChooser extends RandomStringChooser
 {
 	
-	public RandomLetterChooser(String str)
-	{ 
-		/* to be implemented in part (b) */
-		
-	}
+public class RandomLetterChooser extends RandomStringChooser {
+
+    public RandomLetterChooser( String str ) {
+        super( getSingleLetters( str ) );
+    }
+
+    public static String[] getSingleLetters( String str ) {
+        String[] out = new String[str.length()];
+        
+        for (int i=0; i<str.length(); i++) {
+            out[i] = str.substring(i, i+1); 
+        }
+        
+        return out; 
+    }
+
+}
 	
 	
 	public static String[] getSingleLetters(String str)
